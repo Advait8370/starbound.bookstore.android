6ba1dc755512a6830842ba2f29987a78ac089667
Advait <advaitchithira@gmail.com>
2026-06-06 09:17:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,167 +1,76 @@
-// Starbound Stories PWA
-// sw.js
-
-const CACHE_NAME = "starbound-stories-v1";
+const CACHE_NAME = "starbound-stories-v2";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./reader.html",
   "./style.css",
   "./script.js",
-  "./reader.js",
   "./manifest.json",
-
-  "./images/favicon.png",
-  "./images/favicon.ico",
-  "./images/logo.png"
+  "./reader.html",
+  "./reader.js"
 ];
 
-/* -----------------------
-   Install
------------------------ */
+self.addEventListener("install", event => {
+  self.skipWaiting();
 
-self.addEventListener(
-  "install",
-  event => {
-
-    self.skipWaiting();
-
-    event.waitUntil(
-      caches
-      .open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(
-          APP_SHELL
-        );
-      })
-    );
-  }
-);
-
-/* -----------------------
-   Activate
------------------------ */
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-
-      caches.keys().then(
-        keys => {
-
-          return Promise.all(
-
-            keys.map(key => {
-
-              if (
-                key !== CACHE_NAME
-              ) {
-
-                return caches.delete(
-                  key
-                );
-
-              }
-
-            })
-
-          );
-
-        }
-
-      )
-
-    );
-
-    self.clients.claim();
-
-  }
-);
-
-/* -----------------------
-   Fetch
------------------------ */
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    // Cache books.json and images from GitHub
-
-    if (
-      event.request.url.includes(
-        "advait8370.github.io/Starbound-Stories-Bookstore"
-      )
-    ) {
-
-      event.respondWith(
-
-        caches.match(
-          event.request
-        ).then(
-          cached => {
-
-            return (
-              cached ||
-              fetch(
-                event.request
-              ).then(
-                response => {
-
-                  const copy =
-                  response.clone();
-
-                  caches
-                  .open(
-                    CACHE_NAME
-                  )
-                  .then(
-                    cache => {
-                      cache.put(
-                        event.request,
-                        copy
-                      );
-                    }
-                  );
-
-                  return response;
-
-                }
-              )
-            );
-
-          }
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache =>
+        Promise.all(
+          APP_SHELL.map(file =>
+            cache.add(file).catch(() => {})
+          )
         )
-
-      );
-
-      return;
-
-    }
-
-    // Normal App Cache
-
-    event.respondWith(
-
-      caches.match(
-        event.request
-      ).then(
-        cached => {
-
-          return (
-            cached ||
-            fetch(
-              event.request
-            )
-          );
-
-        }
       )
+  );
+});
 
-    );
+self.addEventListener("activate", event => {
 
-  }
-);
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(cached => {
+
+        return cached || fetch(event.request)
+          .then(response => {
+
+            if (
+              response.status === 200 &&
+              event.request.method === "GET"
+            ) {
+              const copy = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then(cache =>
+                  cache.put(event.request, copy)
+                );
+            }
+
+            return response;
+
+          })
+          .catch(() => cached);
+
+      })
+
+  );
+
+});
